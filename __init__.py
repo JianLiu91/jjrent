20f84de01f3869341306854e6d9bdd62ddc62504
cc <398436357@qq.com>
2018-09-10 11:10:43 +0800

    basic

--- __init__.py
+++ __init__.py
@@ -36,16 +36,40 @@ def jsondata():
     offset = info.get('offset', 0)
 
     method = info.get('method')
-    area = info.get('area')
-    subway = info.get('subway')
+    m_area = info.get('area')
+    m_subway = info.get('subway')
     suboption = info.get('suboption')
     zffs = info.get('zffs')
+
+    print method, m_area, m_subway, suboption, zffs
+
+    sqlscript = 'SELECT * from HOUSE where 1=1 '
+
+    if not (suboption == u'不限' or len(suboption.strip()) == 0):
+        sqlscript += " and TITLE GLOB '*%s*' " % (suboption)
+    
+    elif m_area != u'不限':
+        temp = ' and ( '
+        m_area = m_area.encode('utf8')
+        all_area = ["TITLE GLOB '*%s*'" % t for t in area[m_area]]
+        sql_area = " or ".join(all_area)
+        sqlscript += temp + sql_area + ')'
+        
+    elif m_subway != u'不限':
+        pass
+
+
+    if zffs != u'不限':
+        zffs = zffs[0]
+        sqlscript = sqlscript + " and TITLE GLOB '*%s*' " % zffs
+
+    print sqlscript
 
     data = []
     conn = sqlite3.connect('db/test.db')
     c = conn.cursor()
 
-    cursor = c.execute("SELECT * from HOUSE")
+    cursor = c.execute(sqlscript)
     for row in cursor:
         d = {}
         d['source'], d['title'], d['user'], d['post_time'] = row[0], {'href':row[1], 'title': row[2]}, row[3], row[4]
--- db/db_select.py
+++ db/db_select.py
@@ -6,7 +6,7 @@ conn = sqlite3.connect('test.db')
 print "Opened database successfully";
 c = conn.cursor()
 
-cursor = c.execute("SELECT * from HOUSE")
+cursor = c.execute(u"SELECT * from HOUSE where TITLE glob '*东城*' or TITLE glob '*西城*'")  # or '*西城*'")
 for row in cursor:
     print row
 
