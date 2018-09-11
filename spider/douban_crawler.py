4f2f16bb4c8d0dcb6ed0d050a0889d6bde151979
cc <398436357@qq.com>
2018-09-11 21:30:20 +0800

    test

--- spider/douban_crawler.py
+++ spider/douban_crawler.py
@@ -11,7 +11,7 @@ logging.basicConfig(filename='z_douban.log', format='%(asctime)s - %(message)s',
 
 headers = {
     'content-type': 'application/json',
-    'cookie': 'bid=E6H68TbdrsQ; ps=y; ap_v=0,6.0; __yadk_uid=Ddm2MGllmSBhAEAT66BMTv4bAiRgHvFP; douban-fav-remind=1; ll="108288"; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1536630183%2C%22https%3A%2F%2Faccounts.douban.com%2Fsafety%2Funlock_sms%2Fresetpassword%3Fconfirmation%3Dc5ffaf65486e8d57%26alias%3D%22%5D; ue="ryanliu1991@gmail.com"; dbcl2="67739701:5auzbbYbvfY"; ck=3Dca; __utmt=1; _pk_id.100001.8cb4=16592f55b1f77443.1536626353.2.1536630213.1536626971.; _pk_ses.100001.8cb4=*; push_noty_num=0; push_doumail_num=0; __utma=30149280.740421685.1536626354.1536626354.1536630207.2; __utmb=30149280.21.0.1536630213038; __utmc=30149280; __utmz=30149280.1536626354.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=30149280.6773',
+    'cookie': '',
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'
     }
 
