3a86d01abd432f63ac9c502293e0ca40cd1f4bd5
cc <398436357@qq.com>
2018-09-11 22:49:56 +0800

    test

--- spider/proxy.py
+++ spider/proxy.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def get_proxies():
-    url = 'http://www.xicidaili.com/'
+    url = 'http://www.xicidaili.com/wn/'
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
