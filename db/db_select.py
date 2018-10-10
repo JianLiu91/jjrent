71f1d0edbd4c0835f3fc819da2795427ef567d35
ryan <ryanliu1991@gmail.com>
2018-10-10 17:49:59 +0800

    test

--- db/db_select.py
+++ db/db_select.py
@@ -15,20 +15,22 @@ conn = sqlite3.connect('test.db')
 print "Opened database successfully";
 c = conn.cursor()
 
-sqlscript = "SELECT * FROM HOUSE ORDER BY POST_TIME DESC ;"
+sqlscript = "SELECT * FROM COMMENTS"
 cursor = c.execute(sqlscript).fetchall()
-
 for elem in cursor:
-    temp = ' '.join([x.strip() for x in  elem[4].split()])
-    item = {
-        'source': elem[0],
-        'url': elem[1],
-        'title': elem[2],
-        'user': elem[3],
-        'post_time': datetime.datetime.strptime(temp, '%Y-%m-%d %H:%M:%S'),
-        'crawl_time': datetime.datetime.strptime(elem[5], '%Y-%m-%d %H:%M:%S')
-    }
-    db.rent.update({'title':item['title']}, {"$set": item}, upsert=True)
+    print elem
+
+# for elem in cursor:
+#     temp = ' '.join([x.strip() for x in  elem[4].split()])
+#     item = {
+#         'source': elem[0],
+#         'url': elem[1],
+#         'title': elem[2],
+#         'user': elem[3],
+#         'post_time': datetime.datetime.strptime(temp, '%Y-%m-%d %H:%M:%S'),
+#         'crawl_time': datetime.datetime.strptime(elem[5], '%Y-%m-%d %H:%M:%S')
+#     }
+#     db.rent.update({'title':item['title']}, {"$set": item}, upsert=True)
 
 conn.commit()
 conn.close()
